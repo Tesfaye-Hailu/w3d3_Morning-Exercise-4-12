cc3effcd313c9f96741ba028a4ef22518d67cd4b
Tesfaye Hailu <thailu@LABA.PSWAS.LOCAL>
2023-04-12 11:16:00 -0700

    second commit

--- w3d3.js
+++ w3d3.js
@@ -8,15 +8,36 @@ let getAndReturnPeople = async () => {
 
 
 
+    // const doProblem = async () => {
+    //     let peopleArray = await getAndReturnPeople();
+        //console.log(peopleArray);
+      //  for ( let i = 0; i< peopleArray.length;i++){
+       //     peopleArray[i].name
+           // console.log(peopleArray[i].name)
+        // }
+             // do your work here inside this function
+    //}
+    // doProblem()
+
+          //Easy_Loop through this array of objects and and log the name of each person
+
+          // Medium - Loop through this array and make a new array that only contains people that are shorter than 170cm
+
+    
+       
     const doProblem = async () => {
         let peopleArray = await getAndReturnPeople();
         //console.log(peopleArray);
-        for ( let i = 0; i< peopleArray.length;i++){
-            peopleArray[i].name
-            console.log(peopleArray[i].name)
-        }
+
+      for (let i = 0; i < peopleArray.length; i++) {
+  if (peopleArray[i].height < 170) {
+    
+    console.log(peopleArray[i].name);
+        
              // do your work here inside this function
     }
-    doProblem()
+      }
+    }
+   doProblem()
 
-          //Easy_Loop through this array of objects and and log the name of each person
+
